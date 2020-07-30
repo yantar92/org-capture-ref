0adfa109e05cb785381132a6db17b39394073186
Ihor Radchenko <yantar92@gmail.com>
2020-07-30 18:20:19 +0800

    Initial commit

---
+++ org-capture-bib.el
@@ -0,0 +1,61 @@
+;;; org-capture-bib.el --- Capture websites based on their bibtex info  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Ihor Radchenko
+
+;; Author: Ihor Radchenko <yantar92@gmail.com>
+;; Keywords: tex, multimedia, bib
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a wrapper to `org-capture-templates' that automatically
+;; extracts useful meta-information from the captured URLs. The
+;; information is saved into bitex entry and can be reused to fill the
+;; capture template.
+
+;;; Code:
+
+(require 'org-capture)
+(require 'org-ref-url-utils)
+
+(defvar org-capture-bib--store-link-plist nil
+  "A copy of `org-store-link-plist'.
+The following keys are recognized by generic parser (though all
+available keys can be accessed by user-defined parsers):
+:link                 Captured link
+:description          Page title, as given to org-capture
+:query                Query provided to `org-protocol-capture'. The following special fields are recognized:
+  :html               Path to html file containing the page. Providing
+                      this will speed up processing since there will be no need to download
+                      the link contents.
+  :qutebrowser-fifo   Path to FIFO communicating with qutebrowser instance
+  :elfeed-data        Elfeed entry containing the information about captured URL.")
+
+(defvar org-capture-bib--html-buffer nil
+  "Buffer containing downloaded webpage being captured.")
+
+(defvar org-capture-bib--bibtex nil
+  "Alist containing bibtex fields for the webpage being captured.
+The fields include:
+:author       - the author of the URL contents
+:title        - title of the URL contents
+:url          - cleaned-up URL
+:year         - publication year
+:urldate      - capture time
+:journal      - journal name (for journal articles and books)
+:howpublished - website name (for generic URLs).")
+
+(provide 'org-capture-bib)
+;;; org-capture-bib.el ends here
